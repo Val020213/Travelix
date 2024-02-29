b374e8f9a0361d780161917c933962ed35b5812c
Phann020126 <daniel020126@gmail.com>
2024-02-29 00:41:24 -0500

    delete crud

--- app/db/crud.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy.orm import Session
-from db.models import AgencyModel
-from schemas.schema import AgencySchema
-from db.models import UserModel
-
-def get_agencies(db:Session,skip:int=0,limit:int=100):
-    return db.query(AgencyModel).offset(skip).limit(limit).all()
