9ed156775a2e303d88cd2411d9ce4400c4095dc4
Osvaldo R.Moreno Prieto <osvaldo020213.com>
2024-02-28 22:55:00 -0500

    feat/.env bd password and link to docs

--- app/db/config.py
+++ app/db/config.py
@@ -1,12 +1,21 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/travelixdb"
+# Load the environment variables from .env file
+load_dotenv()
+
+# Get the password from the environment variable, or use the default value "postgres"
+password = os.getenv("PASSWORD", "postgres")
+
+DATABASE_URL = f"postgresql://postgres:{password}@localhost:5432/travelixdb"
 
 Base = declarative_base()
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 
 def get_db():
     db = SessionLocal()
