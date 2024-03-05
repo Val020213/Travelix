7e8257412d5adfc9ebff4fc694284d636efc5e64
Phann020126 <daniel020126@gmail.com>
2024-03-05 14:17:35 -0500

    add midelware cors

--- app/main.py
+++ app/main.py
@@ -3,11 +3,20 @@ from db.config import engine
 from api.routes import routers
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
+
 
 
 app = FastAPI()
 app.include_router(routers)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def Home():
